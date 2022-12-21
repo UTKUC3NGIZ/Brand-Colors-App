9cd9a5b785a46a039a40d77e700f9e146b1ae605
Utku Cengiz <cekicengiz01@gmail.com>
2022-12-21 07:48:38 +0300

    Codes that allow expansion and color codes to appear when brands are clicked

--- src/components/Brand.js
+++ src/components/Brand.js
@@ -1,10 +1,24 @@
-import React from "react";
+import { useContext, useState } from "react";
 import { getContrastYIQ } from "../helpers";
+import MainContext from "../MainContext";
 
 function Brand({ brand }) {
+  const { selectedBrands, setSelectedBrands } = useContext(MainContext);
+
+  const toggleSelected = () => {
+    if (selectedBrands.includes(brand.slug)) {
+      setSelectedBrands(selectedBrands.filter((slug) => slug !== brand.slug));
+    } else {
+      setSelectedBrands([...selectedBrands, brand.slug]);
+    }
+  };
   return (
-    <div className="brand">
-      <h5>{brand.title}</h5>
+    <div
+      className={`brand ${
+        selectedBrands.includes(brand.slug) ? "selected" : ""
+      }`}
+    >
+      <h5 onClick={toggleSelected}>{brand.title}</h5>
       <div className="brand-colors">
         {brand.colors.map((color) => (
           <span
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,18 +1,10 @@
-import React, { useState } from "react";
 import Search from "./Search";
-import BrandsData from "../brands.json";
 import Brand from "./Brand";
+import MainContext from "../MainContext";
+import { useContext } from "react";
 
 function Content() {
-  const brandsArray = [];
-
-  Object.keys(BrandsData).map((key) => {
-    brandsArray.push(BrandsData[key]);
-  });
-
-  const [brands, setBrands] = useState(brandsArray);
-
-  console.log(brandsArray);
+  const { brands } = useContext(MainContext);
   return (
     <main className="content">
       <header className="header">
